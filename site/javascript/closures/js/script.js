784ab47a4c3a4600eafe8d37a3cd5261c2109304
Pamela Chadanian <Pamela Chadanian>
2016-08-14 00:16:17 -0300

    closures

--- site/javascript/closures/js/script.js
+++ site/javascript/closures/js/script.js
@@ -1,1 +1,19 @@
-var x = "Hello World"
+// Closures
+function makeMultiplier (multiplier) {
+  // var multiplier = 2;
+  function b() {
+    console.log("Multiplier is: " + multiplier);
+  }
+  b();
+
+
+  return (
+      function (x) {
+        return multiplier * x;
+      }
+
+    );
+}
+
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(10)); // its own exec env
